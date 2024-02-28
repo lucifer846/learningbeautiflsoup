86b1c845bc7379517401c569a662ac47714f183b
saurabh <kapanarinsal6@gmail.com>
2024-02-28 16:37:22 +0530

    added some

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 url = "https://www.croma.com/phones-wearables/mobile-phones/android-phones/c/95"
 result = requests.get(url)
 soup = BeautifulSoup(result.text, 'html.parser')
-print(soup.prettify())
+rate = soup.find_all(string='₹12,999')
+print(rate)
+for i in range(4):
+    print(rate[i].parent)
