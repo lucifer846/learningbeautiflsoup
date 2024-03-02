414a038436cf23bdf87c1ebd4603647146577123
saurabh <kapanarinsal6@gmail.com>
2024-03-02 23:37:24 +0530

    progress on this phone price web scraper, this commit not tested yet

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 url = "https://www.croma.com/phones-wearables/mobile-phones/android-phones/c/95"
 result = requests.get(url)
 soup = BeautifulSoup(result.text, 'html.parser')
-rate = soup.find_all("li") # the li tags lists all products
-print(rate)
+
+for i in range(11):
+    rate = soup.find_all("li", class_="product-item")[i] # the li tags lists all products
+    #print(rate.prettify())
+    name = rate.find_all("a")[1].text
+    print(name)
