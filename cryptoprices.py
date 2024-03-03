e307bf133932f1737a1052c554ec948ac68728df
saurabh <kapanarinsal6@gmail.com>
2024-03-03 21:45:06 +0530

    find crypto prices upto 10 rows, not more that than because more rows dont load until we scroll the website , will fix that in next commit

---
+++ cryptoprices.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+import sys,io
+
+#set the console encoding to utf-8 to display the indian rupee sign in webpage
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+url = "https://coinmarketcap.com/"
+
+fetch = requests.get(url)
+soup = BeautifulSoup(fetch.text, "html.parser")
+tbody = soup.find("tbody")
+trows = tbody.find_all("tr")
+
+for i in trows:
+    tagwithname = i.find("div", class_="sc-a0353bbc-0 gDrtaY")
+    price = tagwithname.text
+    print(price)
